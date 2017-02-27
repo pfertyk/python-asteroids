ee464b557db7eee84862bb8aa0e8c780fee3b8d7
Paweł Fertyk <pfertyk@openmailbox.org>
2017-02-27 02:04:45 +0100

    Refactor drawing and containing functions

--- main.py
+++ main.py
@@ -16,44 +16,37 @@ IMG_ASTEROID_MEDIUM = pygame.image.load('asteroid-medium.png').convert_alpha()
 IMG_ASTEROID_SMALL = pygame.image.load('asteroid-small.png').convert_alpha()
 IMG_BULLET = pygame.image.load('bullet.png').convert_alpha()
 IMG_STARSHIP = pygame.image.load('starship.png').convert_alpha()
+
 CLOCK = pygame.time.Clock()
+FONT = pygame.font.Font(None, 64)
 
 
-# GAME CLASSES
-class Screen:
-    def __init__(self, screen_surface):
-        self.background = screen_surface
-        self.size = screen_surface.get_size()
-        self.font = pygame.font.Font(None, 64)
+# GAME CLASSES AND METHODS
+def print_message(surface, message, font):
+    if message:
+        size = surface.get_size()
+        text = font.render(message, 1, (200, 200, 0))
+        rect = text.get_rect()
+        rect.center = (size[0] / 2, size[1] / 2)
+        surface.blit(text, rect)
 
-    def draw(self):
-        self.background.blit(IMG_EARTH, (0, 0))
 
-    def draw_object(self, obj):
-        obj.draw(self.background)
+class Cosmos:
+    def __init__(self, size):
+        self.size = size
 
-    def print(self, text):
-        if text:
-            text = self.font.render(text, 1, (200, 200, 0))
-            rect = text.get_rect()
-            rect.center = (self.size[0]/2, self.size[1]/2)
-            screen.background.blit(text, rect)
+    def draw(self, surface):
+        surface.blit(IMG_EARTH, (0, 0))
 
     def contain_object(self, obj):
-        pos = obj.pos
-        orig_pos = pos[:]
-        if pos[0] > self.size[0]:
-            pos[0] = pos[0] - self.size[0]
-        if pos[1] > self.size[1]:
-            pos[1] = pos[1] - self.size[1]
-        if pos[0] < 0:
-            pos[0] = pos[0] + self.size[0]
-        if pos[1] < 1:
-            pos[1] = pos[1] + self.size[1]
-        return pos != orig_pos
+        orig_pos = tuple(obj.pos)
+        x, y = obj.pos
+        w, h = self.size
+        obj.pos = (x % w, y % h)
+        return obj.pos != orig_pos
 
 
-class Object:
+class GameObject:
     def __init__(self, pos, radius, speed, angle):
         self.pos = pos
         self.dir = [speed*math.cos(angle), speed*math.sin(angle)]
@@ -68,13 +61,13 @@ class Object:
 
     def collides_with(self, other_obj):
         distance = math.sqrt(
-            (self.pos[0]-other_obj.pos[0])**2 +
-            (self.pos[1]-other_obj.pos[1])**2
+            (self.pos[0] - other_obj.pos[0])**2 +
+            (self.pos[1] - other_obj.pos[1])**2
         )
         return distance < self.radius + other_obj.radius
 
 
-class Asteroid(Object):
+class Asteroid(GameObject):
     def __init__(self, pos, image=IMG_ASTEROID_BIG, radius=50, speed=3.0):
         self.image = image
         angle = 2 * math.pi * random.random()
@@ -93,22 +86,22 @@ class Asteroid(Object):
             return None
 
 
-class Bullet(Object):
+class Bullet(GameObject):
     image = IMG_BULLET
 
     def __init__(self, pos, angle, radius=5, speed=8.0):
         super().__init__(pos, radius, speed, angle)
 
 
-class Starship(Object):
+class Starship(GameObject):
     def __init__(self, pos):
         super().__init__(pos, 20, 0.0, 0)
         self.angle = -90.0
         self.acc = 0.4
 
     def draw(self, surface):
-        sur = pygame.transform.rotozoom(IMG_STARSHIP, -self.angle-90.0, 1.0)
-        pos = [int(c-sur.get_width()/2) for c in self.pos]
+        sur = pygame.transform.rotozoom(IMG_STARSHIP, -self.angle - 90.0, 1.0)
+        pos = [int(c - sur.get_width() / 2) for c in self.pos]
         surface.blit(sur, pos)
 
     def rotate(self, clockwise=True):
@@ -120,7 +113,7 @@ class Starship(Object):
         self.dir[1] += math.sin(self.angle / 180.0 * math.pi) * self.acc
 
 # GAME INIT
-screen = Screen(SCREEN)
+cosmos = Cosmos(SCREEN.get_size())
 starship = Starship([400, 300])
 
 asteroids = []
@@ -131,7 +124,7 @@ status_text = ''
 
 for _ in range(6):
     angle = 2 * math.pi * random.random()
-    pos = [400+300*math.sin(angle), 300+300*math.cos(angle)]
+    pos = [400 + 300 * math.sin(angle), 300 + 300 * math.cos(angle)]
     asteroids.append(Asteroid(pos))
 
 while not done:
@@ -145,7 +138,7 @@ while not done:
             elif event.key == pygame.K_SPACE and starship:
                 angle = starship.angle / 180.0 * math.pi
                 bullets.append(Bullet(starship.pos, angle))
-    # CONTROL
+    # LOGIC
     if starship:
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             starship.rotate(True)
@@ -153,12 +146,12 @@ while not done:
             starship.rotate(False)
         if pygame.key.get_pressed()[pygame.K_UP]:
             starship.move()
-        screen.contain_object(starship)
+        cosmos.contain_object(starship)
 
     for asteroid in asteroids:
-        screen.contain_object(asteroid)
+        cosmos.contain_object(asteroid)
         for bullet in bullets:
-            if screen.contain_object(bullet):
+            if cosmos.contain_object(bullet):
                 bullets.remove(bullet)
                 continue
             if bullet.collides_with(asteroid):
@@ -175,11 +168,11 @@ while not done:
             starship = None
             status_text = "You lost!"
     # DRAWING
-    screen.draw()
+    cosmos.draw(SCREEN)
     for obj in chain(asteroids, bullets, (starship, ) if starship else ()):
         obj.animate()
-        screen.draw_object(obj)
-    screen.print(status_text)
+        obj.draw(SCREEN)
+    print_message(SCREEN, status_text, FONT)
 
     pygame.display.flip()
     CLOCK.tick(60)
